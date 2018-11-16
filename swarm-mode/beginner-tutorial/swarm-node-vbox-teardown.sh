872398a4e7d1558abe8c48002c395fe9188c01fb
Robert Rettig <robert.rettig@ndgit.com>
2018-11-17 00:26:57 +0100

    MOD FOR LOCAL USAGE WITH VIRTUALBOX

--- swarm-mode/beginner-tutorial/swarm-node-vbox-teardown.sh
+++ swarm-mode/beginner-tutorial/swarm-node-vbox-teardown.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+MACHINE_STORAGE_PATH="${PWD}/.docker/machine"
+export MACHINE_STORAGE_PATH
 ### Warning: This will remove all docker machines running ###
 # Stop machines
 docker-machine stop $(docker-machine ls -q)
 
 # remove machines
-docker-machine rm $(docker-machine ls -q)
+#docker-machine rm $(docker-machine ls -q)
