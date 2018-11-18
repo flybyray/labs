b959d9c5ec3f34d330ac4aef46aac8bc8e91fae5
Robert Rettig <robert.rettig@ndgit.com>
2018-11-18 15:25:01 +0100

    allow custom machine names speed up setup

--- swarm-mode/beginner-tutorial/swarm-node-vbox-setup.sh
+++ swarm-mode/beginner-tutorial/swarm-node-vbox-setup.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+# BEGIN DECLARATIONS
 MACHINE_STORAGE_PATH="${PWD}/.docker/machine"
 VIRTUALBOX_CPU_COUNT=2
 VIRTUALBOX_MEMORY_SIZE=2048
 export MACHINE_STORAGE_PATH VIRTUALBOX_CPU_COUNT VIRTUALBOX_MEMORY_SIZE
 
+declare -a SERVER_NAMEN
+declare -a managers
+declare -a workers
+
+########################################################################################################################
+#BEGIN VARIABLES                                                                                                       #
+########################################################################################################################
+
+SERVER_NAMEN=( manager1 manager2 manager3 worker1 worker2 worker3 ) ; export SERVER_NAMEN
+managers=( 0 1 2 ) ; export managers 
+workers=( 3 4 5 ) ; export workers
+
+# END DECLARATIONS
+
 # Swarm mode using Docker Machine
 
-managers=3
-workers=3
-
 # create manager machines
-echo "======> Creating $managers manager machines ...";
-for node in $(seq 1 $managers);
+echo "======> Creating ${#managers[@]} manager machines ...";
+for node in ${!managers[@]};
 do
 	(
-	echo "======> Creating manager$node machine ...";
-	docker-machine create -d virtualbox manager$node;
+	echo "======> Creating ${SERVER_NAMEN[${managers[$node]}]} as manager machine ...";
+	docker-machine create -d virtualbox ${SERVER_NAMEN[${managers[$node]}]};
 	) &
     managers_pids[${node}]=$!
 done
 
 # create worker machines
-echo "======> Creating $workers worker machines ...";
-for node in $(seq 1 $workers);
+echo "======> Creating ${#managers[@]} worker machines ...";
+for node in ${!workers[@]};
 do
 	(
-	echo "======> Creating worker$node machine ...";
-	docker-machine create -d virtualbox worker$node;
+	echo "======> Creating ${SERVER_NAMEN[${workers[$node]}]} as worker machine ...";
+	docker-machine create -d virtualbox ${SERVER_NAMEN[${workers[$node]}]};
 	) &
     workers_pids[${node}]=$!
 done
@@ -48,42 +63,47 @@ docker-machine ls
 
 # initialize swarm mode and create a manager
 echo "======> Initializing first swarm manager ..."
-docker-machine ssh manager1 "docker swarm init --listen-addr $(docker-machine ip manager1) --advertise-addr $(docker-machine ip manager1)"
+SWARM_MANAGER1_NAME="${SERVER_NAMEN[${managers[0]}]}" ; export SWARM_MANAGER1_NAME
+SWARM_MANAGER1_IP="$(docker-machine ip ${SWARM_MANAGER1_NAME})" ; export SWARM_MANAGER1_IP
+docker-machine ssh ${SWARM_MANAGER1_NAME} "docker swarm init --listen-addr ${SWARM_MANAGER1_IP} --advertise-addr ${SWARM_MANAGER1_IP}"
 
 # get manager and worker tokens
-export manager_token=`docker-machine ssh manager1 "docker swarm join-token manager -q"`
-export worker_token=`docker-machine ssh manager1 "docker swarm join-token worker -q"`
+export manager_token=`docker-machine ssh ${SWARM_MANAGER1_NAME} "docker swarm join-token manager -q"`
+export worker_token=`docker-machine ssh ${SWARM_MANAGER1_NAME} "docker swarm join-token worker -q"`
 
 echo "manager_token: $manager_token"
 echo "worker_token: $worker_token"
 
 # other masters join swarm
-for node in $(seq 2 $managers);
+managers_slice=( "${managers[@]:1}" )
+for node in ${!managers_slice[@]};
 do
-	echo "======> manager$node joining swarm as manager ..."
-	docker-machine ssh manager$node \
-		"docker swarm join \
-		--token $manager_token \
-		--listen-addr $(docker-machine ip manager$node) \
-		--advertise-addr $(docker-machine ip manager$node) \
-		$(docker-machine ip manager1)"
+	echo "======>  ${SERVER_NAMEN[${managers_slice[$node]}]} joining swarm as manager ..."
+	swarm_host_name="${SERVER_NAMEN[${managers_slice[$node]}]}"
+	docker-machine ssh ${swarm_host_name} \
+	"docker swarm join \
+	--token $manager_token \
+	--listen-addr $(docker-machine ip ${swarm_host_name}) \
+	--advertise-addr $(docker-machine ip ${swarm_host_name}) \
+	${SWARM_MANAGER1_IP}"
 done
 
 # show members of swarm
-docker-machine ssh manager1 "docker node ls"
+docker-machine ssh "${SWARM_MANAGER1_NAME}" "docker node ls"
 
 # workers join swarm
-for node in $(seq 1 $workers);
+for node in ${!workers[@]};
 do
-	echo "======> worker$node joining swarm as worker ..."
-	docker-machine ssh worker$node \
+	echo "======>  ${SERVER_NAMEN[${workers[$node]}]} joining swarm as worker ..."
+	swarm_host_name="${SERVER_NAMEN[${workers[$node]}]}"
+	docker-machine ssh ${swarm_host_name} \
 	"docker swarm join \
 	--token $worker_token \
-	--listen-addr $(docker-machine ip worker$node) \
-	--advertise-addr $(docker-machine ip worker$node) \
-	$(docker-machine ip manager1)"
+	--listen-addr $(docker-machine ip ${swarm_host_name}) \
+	--advertise-addr $(docker-machine ip ${swarm_host_name}) \
+	${SWARM_MANAGER1_IP}"
 done
 
 # show members of swarm
-docker-machine ssh manager1 "docker node ls"
+docker-machine ssh "${SWARM_MANAGER1_NAME}" "docker node ls"
 
--- swarm-mode/beginner-tutorial/swarm-node-vbox-teardown.sh
+++ swarm-mode/beginner-tutorial/swarm-node-vbox-teardown.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-MACHINE_STORAGE_PATH="${PWD}/.docker/machine"
-export MACHINE_STORAGE_PATH
+MACHINE_STORAGE_PATH=".docker/machine" ; export MACHINE_STORAGE_PATH
 ### Warning: This will remove all docker machines running ###
 # Stop machines
 docker-machine stop $(docker-machine ls -q)
 
 # remove machines
-#docker-machine rm $(docker-machine ls -q)
+docker-machine rm $(docker-machine ls -q)
